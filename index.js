81790b3db70a3762a4c996082fc20a32c98153d5
th12legion <th12legion@dev.com>
2020-11-25 10:50:47 +0200

    v2.0.0

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-var config =  require("../../config.json");
+var fs = require("fs");
+var path =  require("path");
+var config =  {};
+var config_path = path.resolve(__dirname+"/../../config.js");
+
+var start_require = function(){
+	try {	
+		if (fs.existsSync(config_path) ){
+			config = require(config_path);
+		}
+	} catch(err) {
+		console.error(err);
+	}
+
+	config['reload'] =  function(path){
+		config_path = path;
+		start_require();
+		
+		return config;
+	}
+}
+
+start_require();
+
 module.exports = config;
